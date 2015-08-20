0158bfb2baa72925ff2d227ce2b18f33195ae76a
Lazzle <lazzlepants@gmail.com>
2015-08-20 17:36:24 +0100

    attempted fix and tidy of select track follows active item ME

--- Selected track follows active item ME.lua
+++ Selected track follows active item ME.lua
@@ -5,14 +5,13 @@ function doIt()
   local ame=reaper.MIDIEditor_GetActive()
   local mode=reaper.MIDIEditor_GetMode(ame)
   if mode > -1 then -- we are in a MIDI editor, -1 if ME not focused
+    
     tk=reaper.MIDIEditor_GetTake(ame)
 	if last_take~=nil then 
 	  if last_take~=tk then -- sometimes crashes on undo
+    --reaper.adjustZoom(10, 1, false, -1) --worth a try :)
 		tr=reaper.GetMediaItemTake_Track(tk)
-		if reaper.CountSelectedTracks(0)>0 then
-			reaper.Main_OnCommand(40297,0)
-		end
-		reaper.SetTrackSelected(tr, true)
+		reaper.SetOnlyTrackSelected(tr)
 	  end
 	end
 	last_take=tk
