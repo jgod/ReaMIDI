9590f515b8eeb51b14aa4161cbbfab274a5fe1b4
Lazzle <lazzlepants@gmail.com>
2015-09-22 22:58:16 +0100

    added setEvent and setEvents.  Got getEvents functional.

--- requires/midi.lua
+++ requires/midi.lua
@@ -39,49 +39,16 @@ types = {
   programchange=0xC0,
   chanpressure=0xD0,
   pitchbend=0xE0,
-  sysex=0xF0,
-  note=-1
+  sysex=-1, --message is 0xF0, but this is what MIDI_SetTextSysexEvt needs
+  note=-2,
+  all=-100
 }
 
 
-function DBG(msg)
+function DBG(dbg_msg)
   if _DBG==true then
-    reaper.ShowConsoleMsg(msg.."\n")
-  end
-end
-
-function midicmp(a,b)
-  if a.startpos ~= b.startpos then return a.startpos < b.startpos end
-  return a.pitch > b.pitch
-end  
-
-function getEvents(takes)
-  local ni, tr, it, tk
-  local midi={}
-  local e_cnt,n_cnt,c_cnt=0
-  for i=1,#takes,1 do
-    tk=takes[i]
-    local msg_sz=""
-    local msg=""
-    local ok, sel, mute, startpos, msg, msg_sz=reaper.MIDI_GetEvt(tk, e_cnt, true, true,1, msg)
-    while ok do
-      startpos=reaper.MIDI_GetProjQNFromPPQPos(tk, startpos)
-      local t=string.byte(msg:sub(1,1))
-      local t1=string.byte(msg:sub(2,2))
-      
-      if t1~=nil then
-        --
-      end
-     
-      t=t&0xF0
-      if t~=0xF0 then chan=t&0x0F end
-
-      local event={ type=t, track=tr, take=tk, idx=e_cnt, select=sel, mute=mute, startpos=startpos,
-                    msg=msg, msg_sz=msg_sz}
-      midi[#midi+1]=event
-      e_cnt=e_cnt+1
-      ok, sel, mute, startpos, msg, msg_sz=reaper.MIDI_GetEvt(tk, e_cnt, true, true,1, msg)
-    end
+    if dbg_msg==nil then dbg_msg="nil" end
+    reaper.ShowConsoleMsg(dbg_msg.."\n")
   end
 end
 
@@ -101,10 +68,226 @@ function deepcopy(orig)
     return copy
 end
 
+
+function midicmp(a,b)
+  if a.startpos ~= b.startpos then return a.startpos < b.startpos end
+  return a.pitch > b.pitch
+end
+
+function midicmptype(a,b)
+  if a.startpos ~= b.startpos then return a.startpos < b.startpos end
+  return a.e_type > b.e_type
+end
+
+
+function bit_7to14(msb,lsb)
+  local val=((msb<<7)+lsb)-0x1FFF
+  return val
+end
+
+
+function bit_14to7(val)
+  val=tonumber(val)+0x1FFF
+  local msb=(val&0x3F80)>>7 
+  local lsb=val&0x7F
+  return msb, lsb
+end
+
+
+function bytesToString(bytes)
+  local buf=string.rep(' ', #bytes)
+  local i=0
+  return (string.gsub(buf, '(.)', 
+      function(c)
+        i=i+1
+        return string.char(bytes[i])
+      end))
+end
+
+
+
+function setEvent(evt)
+  local m=math
+  if evt.e_type==types.cc then
+    reaper.MIDI_SetCC(evt.tk, evt.idx, evt.sel, evt.mute,
+                       reaper.MIDI_GetPPQPosFromProjQN(evt.tk, evt.startpos),
+                       nil, evt.chan, m.floor(evt.msg2), m.floor(evt.msg3), true)
+    return
+  end
+  if evt.e_type==types.note then 
+    reaper.MIDI_SetNote(evt.tk, evt.idx,evt.sel,evt.mute,
+      reaper.MIDI_GetPPQPosFromProjQN(evt.tk, evt.startpos),
+      reaper.MIDI_GetPPQPosFromProjQN(evt.tk, evt.startpos+evt.len),evt.chan,
+      m.floor(evt.pitch),m.floor(evt.vel),nil)
+    return
+   end
+   if evt.e_type==types.sysex or evt.e_type>0 and evt.e_type<=7 then
+     reaper.MIDI_SetTextSysexEvt(evt.tk,evt.idx,evt.sel,evt.mute,
+        reaper.MIDI_GetPPQPosFromProjQN(evt.tk, evt.startpos),
+        nil, evt.str, true)
+     return
+   end
+   if evt.e_type==types.pitchbend then
+     local msg3, msg2=bit_14to7(math.floor(evt.amount))
+     local msg=bytesToString({evt.e_type+evt.chan,msg2,msg3})
+     reaper.MIDI_SetEvt(evt.tk,evt.idx,evt.sel,evt.mute,
+               reaper.MIDI_GetPPQPosFromProjQN(evt.tk, evt.startpos),
+               msg, true)
+     return
+   end
+   if evt.e_type==types.aftertouch or evt.e_type==types.programchange or
+        evt.e_type==types.chanpressure then
+     local msg=bytesToString({evt.e_type+evt.chan,msg2,msg3})
+     reaper.MIDI_SetEvt(evt.tk,evt.idx,evt.sel,evt.mute,
+               reaper.MIDI_GetPPQPosFromProjQN(evt.tk, evt.startpos),
+               msg, true)
+     return
+   end
+end
+
+
+function setEvents(evts)
+  if evts~=nil then
+    for i=#evts,1,-1 do
+      setEvent(evts[i])
+    end 
+  end
+end
+
+
+--_DBG=true
+function getEvents(types_tab, takes,sort,selected)
+  local ni, tr, tr_num, it, tk
+  local midi={}
+  local midi_tk={}
+  
+  for i=1,#takes,1 do
+    local cnt_e,cnt_n,cnt_cc,cnt_tsx=0,0,0,0
+    tk=takes[i]
+    local msg_sz=""
+    local msg=""
+    --CCs
+    local chanmsg,chan,msg2,msg3, idx
+    --notes
+    local endpos,pitch,vel
+    local ok, sel, mute, startpos, msg, msg_sz=reaper.MIDI_GetEvt(tk, cnt_e, true, true,1, msg)
+    while ok do
+      
+      idx=cnt_e
+      DBG("Length of msg: "..string.len(msg))
+      DBG(msg)
+      local t=string.byte(msg:sub(1,1))
+      DBG(t)
+      local t2=string.byte(msg:sub(2,2))
+      DBG(t2)
+      local t3=string.byte(msg:sub(3,3))
+      DBG(t3)
+      chan=t&0x0F 
+      if t&0xF0>=0x80 then
+        DBG("Checking events")
+        e_type=t&0xF0 
+        if e_type==types.cc then
+          DBG("isCC")
+          ok,sel,mute,startpos,chanmsg,chan,msg2,msg3=reaper.MIDI_GetCC(tk,cnt_cc)
+          idx=cnt_cc
+          cnt_cc=cnt_cc+1
+        end
+        
+        if e_type==types.pitchbend then
+          DBG("isPB")
+          DBG("Pitchbend  msg2: "..tonumber(t2).."  msg3: "..tonumber(t3))
+          amount=bit_7to14(tonumber(t3),tonumber(t2))
+          DBG("14 bit: ".. amount)
+          --
+        end
+        
+        if e_type==types.noteon then
+          DBG("isNoteOn")
+          e_type=types.note
+          ok, sel, mute, startpos, endpos, chan, pitch, vel=reaper.MIDI_GetNote(tk, cnt_n)
+          idx=cnt_n
+          cnt_n=cnt_n+1
+          endpos=reaper.MIDI_GetProjQNFromPPQPos(tk, endpos)
+        end
+      end
+      
+      --text and sysex events here
+      if e_type==0xF0 or (t>0 and t<=7) then
+        DBG("isTextorSYSex")
+        if t>0 and t<=7 then e_type=t end
+        if t==0xF0 then e_type=types.sysex end
+        --startpos returning 0 here
+        ok, sel, mute, startpos, m_type, str=reaper.MIDI_GetTextSysexEvt(tk, cnt_tsx,
+                                nil, nil, 0, nil, "")
+        idx=cnt_tsx
+        cnt_tsx=cnt_tsx+1 
+      end
+      
+      startpos=reaper.MIDI_GetProjQNFromPPQPos(tk, startpos)
+      local len
+      if endpos~=nil then len=endpos-startpos end
+      local meas, meas_startpos, meas_end=reaper.TimeMap_QNToMeasures(0,startpos)
+      
+      -- BUG?  Should we have to take 1 away to get correct time sig?
+      local startpos_secs, _,_,num,denom,tempo=reaper.TimeMap_GetMeasureInfo(0, meas-1)
+      local qnpm=num/(denom/4)
+      
+      -- *Probably don't do this*: change time-sig to next measure if note starts really close to it
+      -- TODO:  have a bigger think about actual placement and intended, musical placement
+      if meas_tolerance~=nil then
+        if meas_end-startpos<meas_tolerance then
+          local _,_,_,n,d,_=reaper.TimeMap_GetMeasureInfo(0, meas-1+1)  -- ^^ reminder in case of bug ^^
+          num,denom=n,d
+        end
+      end
+      
+      tr=reaper.GetMediaItemTake_Track(tk)
+      tr_num=reaper.GetMediaTrackInfo_Value(tr, "IP_TRACKNUMBER")
+      
+      if selected==false or (selected==true and sel==true) then 
+        DBG("Adding event to list")
+        
+        local event={ --all
+                      e_type=e_type, track=tr, tk=tk, idx=idx, select=sel, 
+                      mute=mute, startpos=startpos, chan=chan,
+                      --mostly ccs
+                      chanmsg=chanmsg, msg=msg, msg2=msg2, msg3=msg3, msg_sz=msg_sz,
+                      --note specific
+                      endpos=endpos, pitch=pitch, vel=vel,ts_num=num,ts_denom=denom,
+                      len=len,qn_in_meas=qnpm,
+                      --text/sysex
+                      m_type=m_type, str=str,
+                      --pitchbend
+                      amount=amount}
+                      
+        midi[#midi+1]=event
+      end
+      cnt_e=cnt_e+1
+      ok, sel, mute, startpos, msg, msg_sz=reaper.MIDI_GetEvt(tk, cnt_e, true, true,1, msg)
+    end
+    midi_tk[i]=deepcopy(midi)
+    DBG("size of midi_tk (#events): "..#midi_tk[i])
+    table.sort(midi_tk[i], midicmptype)
+    DBG("size of midi_tk (#events): "..#midi_tk[i])
+    midi={}
+  end
+  local nn=1
+  for i=1,#midi_tk,1 do
+    for ii=1,#midi_tk[i],1 do
+      midi[nn]=midi_tk[i][ii]
+      nn=nn+1
+    end
+  end
+  if sort==true then table.sort(midi, midicmptype) end --this now sorts notes by pos/pitch irrespective of what take they are in
+  return midi
+end
+
+
 local meas_tolerance=nil
 function setMeasureTolerance(m)
   meas_tolerance=m
 end
+
 
 function getNotes(takes,sort,selected)
   local ni, tr, tr_num, it, tk
@@ -173,6 +356,18 @@ function getTargetNotes(sel_notes_in_arrange, sel_only_in_ME)
     notes=getNotes(takes, false, false)
   end
   return target, notes
+end
+
+
+function getTargetEvents(types_tab,sel_events_in_arrange, sel_only_in_ME)
+  DBG("in getTargetEvents")
+  local target, takes=getTargetTakes() --from target.lua
+  local events=getEvents(types_tab, takes, false, (target==targets.MIDIEditor) or sel_events_in_arrange)
+  if target==targets.MIDIEditor and #events==0 and not sel_only_in_ME then 
+    events=getEvents(types_tab, takes, false, false)
+  end
+  DBG("about to exit getTargetEvents: "..#events)
+  return target, events
 end
 
 
