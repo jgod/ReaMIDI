cad29b1e63958763b4232621cb0f427a3c589448
Lazzle <lazzlepants@gmail.com>
2015-10-06 22:36:58 +0100

    update GUI test

--- requires/tests/el-gui-test.lua
+++ requires/tests/el-gui-test.lua
@@ -2,7 +2,6 @@ dofile(reaper.GetResourcePath().."\\Scripts\\ReaMIDI\\requires\\el gui.lua")
 
 -- override this function to do something unique
 function LListControl:onDoubleClick(x,y,m_mod)
-  reaper.ShowConsoleMsg("onDoubleCLick\n")
   local ok, str=reaper.GetUserInputs("Rename",1,"Name: ","")
   y=y-self.y
   local row=math.floor((y-self.margin)/self.row_height)+self.first_vis_row
@@ -10,13 +9,12 @@ function LListControl:onDoubleClick(x,y,m_mod)
 end
 
 
+LGUI.init("El GUI Test", 1000, 520, true)
 
-local script_name="El GUI Test"
-LGUI.state_name="I8bE"..script_name
 
 ---[[ --uncomment to delete state (for testing)
 for i=1,10,1 do
-  reaper.DeleteExtState(LGUI.state_name,i,true)
+  reaper.DeleteExtState(LGUI.state_name,tostring(i),true)
 end
 --]]
 function DBG(str)
@@ -25,9 +23,9 @@ end
 
 
 function goToEditMode()
-  DBG("goToEditMode")
+  LGUI.edit_mode=not LGUI.edit_mode
   for i=1,#LGUI.controls,1 do
-    if i~=2 then
+    if LGUI.controls[i].can_edit then 
       LGUI.controls[i].edit_mode=not LGUI.controls[i].edit_mode
     end
   end
@@ -35,9 +33,7 @@ end
   
 
 local editbox
-function init()  
-  gfx.init(script_name, 1000, 520, true)
-  
+function init()
   b=LButton(nil,nil,
                   false,
                   "Hello", 100,20,60,25,
@@ -54,6 +50,7 @@ function init()
                   {0.8,0.8,0.8},
                   nil
                )
+  b1.can_edit=false
   LGUI.addControl(b1)
  
   cb=LCheckBox(250,20,150,10,"Label 1")
@@ -64,7 +61,7 @@ function init()
   LGUI.addControl(cb1)
   cb1:setColour(cb1.colour_bg, 0,0,0)
  
-  llc=LListControl(20,100,400,400,10,
+  llc=LListControl(20,100,200,200,10,
                       "Verdana",
                       {0.2,0.2,0.2},
                       {0.9,0.9,0.9},
@@ -75,9 +72,12 @@ function init()
   
   llc:setColour(llc.colour_fg, 0,0,0)
   
-  editbox=LEditBox(20,400,500,30,50,50)
+  editbox=LEditBox(20,400,500,20,50,50)
   
   LGUI.addControl(editbox)
+  
+  label=LLabel(250,100,100,30,"El GUI Testbed","Arial",30,{.2,0.5,0.5})
+  LGUI.addControl(label)
 end
 
 
@@ -86,15 +86,11 @@ function main()
    gfx.x, gfx.y=0,0
    gfx.r=0xBB/255  gfx.g=0xBF/255   gfx.b=0xBF/255
    gfx.rect(0,0,780,520,true)
-   gfx.x,gfx.y=250,100
-   gfx.r,gfx.g,gfx.b=0,0,0
-   gfx.setfont(1,"Arial", 30)--, "ub")
-   gfx.printf("El GUI Testbed")
    LGUI.process(gfx.getchar(),main)
 end
 
 function onExit()
-  LGUI.onExit()
+  LGUI.saveStates()
 end
 
 reaper.atexit(onExit)
