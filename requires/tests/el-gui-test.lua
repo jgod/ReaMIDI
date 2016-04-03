6634cd64ac3f30a77e40d044ed176041a226dc31
Lazzle <lazzlepants@gmail.com>
2016-04-03 02:15:28 +0100

    adds combo box to test, fixes buttons for new contructor

--- requires/tests/el-gui-test.lua
+++ requires/tests/el-gui-test.lua
@@ -17,17 +17,20 @@ end
 local editbox
 
 function init()
-  LGUI.addControl(LButton(nil,nil,
+  LGUI.addControl(LButton(100,20,60,25,
+                  "Hello", 
+                  nil,nil,
                   false,
-                  "Hello", 100,20,60,25,
+                  
                   {0.2,0.2,0.2},
                   {0.8,0.8,0.8},
                   nil
                ))
   
-  b1=LButton(goToEditMode,nil,
+  b1=LButton(    100,60,60,25,
+                 "Edit",
+                  goToEditMode,nil,
                   true,
-                  "Edit", 100,60,60,25,
                   {0.2,0.2,0.2},
                   {0.8,0.8,0.8},
                   nil
@@ -35,11 +38,11 @@ function init()
   b1.can_edit=false
   LGUI.addControl(b1)
  
-  cb=LCheckBox(250,20,150,10,"Label 1")
+  cb=LCheckBox(250,20,150,10,"Checkbox 1")
   LGUI.addControl(cb)
   cb:setColour(cb.colour_bg, 0,0,0)
  
-  cb1=LCheckBox(250,50,150,10,"Label 2")
+  cb1=LCheckBox(250,50,150,10,"Checkbox 2")
   LGUI.addControl(cb1)
   cb1:setColour(cb1.colour_bg, 0,0,0)
  
@@ -56,6 +59,11 @@ function init()
     local row=math.floor((y-self.margin)/self.row_height)+self.first_vis_row
     if ok then self.state[row][1]=str end
   end
+  
+  function llc:onEnter()
+    local ok, str=reaper.GetUserInputs("Rename",1,"Name: ","")
+    if ok then self.state[self.last_clicked_row][1]=str end
+  end
 
   LGUI.addControl(llc)
   
@@ -69,6 +77,9 @@ function init()
   
   label=LLabel(250,100,100,30,"El GUI Testbed","Arial",30,{.2,0.5,0.5})
   LGUI.addControl(label)
+  
+  combo=LComboBox(250,150,100,20,{"one","two","three"})
+  LGUI.addControl(combo)
   LGUI.process()
 end
 
