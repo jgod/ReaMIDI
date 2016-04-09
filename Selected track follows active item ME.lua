4a3f433ad5ce48a9e22d52efde6e960bf6cd31fd
Lazzle <lazzlepants@gmail.com>
2016-04-09 13:05:15 +0100

    adds safety check for empty ME

--- Selected track follows active item ME.lua
+++ Selected track follows active item ME.lua
@@ -5,11 +5,10 @@ function doIt()
   local ame=reaper.MIDIEditor_GetActive()
   local mode=reaper.MIDIEditor_GetMode(ame)
   if mode > -1 then -- we are in a MIDI editor, -1 if ME not focused
-    
     tk=reaper.MIDIEditor_GetTake(ame)
-	if last_take~=nil then 
+    if not reaper.ValidatePtr(tk, 'MediaItem_Take*') then tk=nil end
+	if last_take~=nil and tk~=nil then 
 	  if last_take~=tk then -- sometimes crashes on undo
-    --reaper.adjustZoom(10, 1, false, -1) --worth a try :)
 		tr=reaper.GetMediaItemTake_Track(tk)
 		reaper.SetOnlyTrackSelected(tr)
 	  end
