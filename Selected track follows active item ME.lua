bfff3ab15e45c013cd653fe9819be8a32a5ac9d6
Lazzle <lazzlepants@gmail.com>
2016-04-09 13:13:25 +0100

    improves performance

--- Selected track follows active item ME.lua
+++ Selected track follows active item ME.lua
@@ -1,21 +1,26 @@
 local last_take=nil
+local every_nth=20 -- don't run 60 times a second
+local counter=every_nth
 
-function doIt()
-  --reaper.ShowConsoleMsg("In doIt()\n")
-  local ame=reaper.MIDIEditor_GetActive()
-  local mode=reaper.MIDIEditor_GetMode(ame)
-  if mode > -1 then -- we are in a MIDI editor, -1 if ME not focused
-    tk=reaper.MIDIEditor_GetTake(ame)
-    if not reaper.ValidatePtr(tk, 'MediaItem_Take*') then tk=nil end
-	if last_take~=nil and tk~=nil then 
-	  if last_take~=tk then -- sometimes crashes on undo
-		tr=reaper.GetMediaItemTake_Track(tk)
-		reaper.SetOnlyTrackSelected(tr)
-	  end
-	end
-	last_take=tk
+function loop()
+  if counter>=every_nth then
+    counter=0
+    local ame=reaper.MIDIEditor_GetActive()
+    local mode=reaper.MIDIEditor_GetMode(ame)
+    if mode > -1 then -- we are in a MIDI editor, -1 if ME not focused
+      tk=reaper.MIDIEditor_GetTake(ame)
+      if not reaper.ValidatePtr(tk, 'MediaItem_Take*') then tk=nil end
+      if last_take~=nil and tk~=nil then 
+        if last_take~=tk then
+          tr=reaper.GetMediaItemTake_Track(tk)
+          reaper.SetOnlyTrackSelected(tr)
+        end
+      end
+      last_take=tk
+    end
   end
-  reaper.defer(doIt)
+  counter=counter+1
+  reaper.defer(loop)
 end
 
-doIt()
+loop()
